bbe07823157cc429da0ba43fb16774e828b14c42
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-09-10 14:19:06 -0500

    added new button divs for AC

--- reactcalculator/src/App.js
+++ reactcalculator/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="container">
-      <div className="wrapper"></div>
+      <div className="wrapper">
+        <div className="screen"></div>
+        <div className="btn light-gray"></div>
+      </div>
     </div>
   );
 }
