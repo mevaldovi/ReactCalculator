956729ce98799ebcd03a92097b96482c0838575d
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-09-10 14:31:00 -0500

    added opereator button funcionality

--- reactcalculator/src/App.js
+++ reactcalculator/src/App.js
@@ -6,7 +6,10 @@ function App() {
     <div className="container">
       <div className="wrapper">
         <div className="screen"></div>
-        <div className="btn light-gray"></div>
+        <div className="btn light-gray" onClick={reset}>AC</div>
+        <div className="btn light-gray" onClick={percent}>%</div>
+        <div className="btn light-gray" onClick={minusPlus}>+/-</div>
+        <div className="btn orange" onClick={operatorType}>/</div>
       </div>
     </div>
   );
