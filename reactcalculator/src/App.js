955b11969dc9fb3fc29d763b9c937f88af2ea617
MARIE VALDOVINOS <MEVALDOVI@GMAIL.COM>
2021-09-20 21:48:06 -0500

    bug was a scoping issue - missing curly brackets

--- reactcalculator/src/App.js
+++ reactcalculator/src/App.js
@@ -62,134 +62,133 @@ function App() {
     if (e?.target.innerText === "=") {
       setTotal(true);
     }
-  };
+    let cal;
+    switch (operator) {
+      case "/":
+        cal = String(parseFloat(preState) / parseFloat(currentState));
+        break;
 
-  let cal;
-  switch (operator) {
-    case "/":
-      cal = String(parseFloat(preState) / parseFloat(currentState));
-      break;
+      case "+":
+        cal = String(parseFloat(preState) + parseFloat(currentState));
+        break;
 
-    case "+":
-      cal = String(parseFloat(preState) + parseFloat(currentState));
-      break;
+      case "X":
+        cal = String(parseFloat(preState) * parseFloat(currentState));
+        break;
 
-    case "X":
-      cal = String(parseFloat(preState) * parseFloat(currentState));
-      break;
-
-    case "-":
-      cal = String(parseFloat(preState) - parseFloat(currentState));
-      break;
-    default:
-      return;
-  }
-  setInput("");
-  setpreState(cal);
-  setcurrentState("");
-}
-
-const minusPlus = () => {
-  //currentState on lines 94, 85, and 97 need to be lit in aqua blue. Why arent they? Let's find out
-  if (currentState.charAt(0) === "-") {
-    setcurrentState(currentState.substring(1));
-  } else {
-    setcurrentState("-" + currentState);
-  }
-};
-
-const percent = () => {
-  preState
-    ? // if there is preState, convert it to a an integer and divide by 100 and multiply that by the preState
-      setcurrentState(String((parseFloat(currentState) / 100) * preState))
-    : //if no preState ,  simply convert string to integer format and divide by 100.
-      setcurrentState(String(parseFloat(currentState) / 100));
-
-  const reset = () => {
-    //set preState to an empty string
-    setpreState("");
+      case "-":
+        cal = String(parseFloat(preState) - parseFloat(currentState));
+        break;
+      default:
+        return;
+    }
+    setInput("");
+    setpreState(cal);
     setcurrentState("");
-    //set currentState to an empty string
-    setInput("0");
-    //setInput to zero
   };
-  return (
-    <div className="container">
-      <div className="wrapper">
-        <div className="screen">
-          {input !== "" || input === "0" ? (
-            <NumberFormat
-              value={input}
-              displayType={"text"}
-              thousandSeparator={true}
-            />
-          ) : (
-            <NumberFormat
-              value={preState}
-              displayType={"text"}
-              thousandSeparator={true}
-            />
-          )}
-        </div>
-        <div className="btn light-gray" onClick={reset}>
-          AC
-        </div>
-        <div className="btn light-gray" onClick={percent}>
-          %
-        </div>
-        <div className="btn light-gray" onClick={minusPlus}>
-          +/-
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          /
-        </div>
-        <div className="btn" onClick={inputNum}>
-          7
-        </div>
-        <div className="btn" onClick={inputNum}>
-          8
-        </div>
-        <div className="btn" onClick={inputNum}>
-          9
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          X
-        </div>
-        <div className="btn" onClick={inputNum}>
-          4
-        </div>
-        <div className="btn" onClick={inputNum}>
-          5
-        </div>
-        <div className="btn" onClick={inputNum}>
-          6
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          +
-        </div>
-        <div className="btn" onClick={inputNum}>
-          1
-        </div>
-        <div className="btn" onClick={inputNum}>
-          2
-        </div>
-        <div className="btn" onClick={inputNum}>
-          3
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          -
-        </div>
-        <div className=" btn zero" onClick={inputNum}>
-          0
-        </div>
-        <div className="btn" onClick={inputNum}>
-          .
-        </div>
-        <div className="btn" onClick={equals}>
-          =
+
+  const minusPlus = () => {
+    //currentState on lines 94, 85, and 97 need to be lit in aqua blue. Why arent they? Let's find out
+    if (currentState.charAt(0) === "-") {
+      setcurrentState(currentState.substring(1));
+    } else {
+      setcurrentState("-" + currentState);
+    }
+  };
+
+  const percent = () => {
+    preState
+      ? // if there is preState, convert it to a an integer and divide by 100 and multiply that by the preState
+        setcurrentState(String((parseFloat(currentState) / 100) * preState))
+      : //if no preState ,  simply convert string to integer format and divide by 100.
+        setcurrentState(String(parseFloat(currentState) / 100));
+
+    const reset = () => {
+      //set preState to an empty string
+      setpreState("");
+      setcurrentState("");
+      //set currentState to an empty string
+      setInput("0");
+      //setInput to zero
+    };
+    return (
+      <div className="container">
+        <div className="wrapper">
+          <div className="screen">
+            {input !== "" || input === "0" ? (
+              <NumberFormat
+                value={input}
+                displayType={"text"}
+                thousandSeparator={true}
+              />
+            ) : (
+              <NumberFormat
+                value={preState}
+                displayType={"text"}
+                thousandSeparator={true}
+              />
+            )}
+          </div>
+          <div className="btn light-gray" onClick={reset}>
+            AC
+          </div>
+          <div className="btn light-gray" onClick={percent}>
+            %
+          </div>
+          <div className="btn light-gray" onClick={minusPlus}>
+            +/-
+          </div>
+          <div className="btn orange" onClick={operatorType}>
+            /
+          </div>
+          <div className="btn" onClick={inputNum}>
+            7
+          </div>
+          <div className="btn" onClick={inputNum}>
+            8
+          </div>
+          <div className="btn" onClick={inputNum}>
+            9
+          </div>
+          <div className="btn orange" onClick={operatorType}>
+            X
+          </div>
+          <div className="btn" onClick={inputNum}>
+            4
+          </div>
+          <div className="btn" onClick={inputNum}>
+            5
+          </div>
+          <div className="btn" onClick={inputNum}>
+            6
+          </div>
+          <div className="btn orange" onClick={operatorType}>
+            +
+          </div>
+          <div className="btn" onClick={inputNum}>
+            1
+          </div>
+          <div className="btn" onClick={inputNum}>
+            2
+          </div>
+          <div className="btn" onClick={inputNum}>
+            3
+          </div>
+          <div className="btn orange" onClick={operatorType}>
+            -
+          </div>
+          <div className=" btn zero" onClick={inputNum}>
+            0
+          </div>
+          <div className="btn" onClick={inputNum}>
+            .
+          </div>
+          <div className="btn" onClick={equals}>
+            =
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 export default App;
